c4eb4344352012fd89f260e9d96b4599a679386a
Fábio Ewerton <fabioew89@gmail.com>
2024-10-09 10:04:57 -0400

    modified:   app/controllers/routes.py

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from app import app
 
 @app.route('/')
+@app.route('/index/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/template')
+def template():
+    return render_template('loginpage.html')
